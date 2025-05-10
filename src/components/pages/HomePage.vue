631994d7dddf72c7c489639fa18f236a5c2a436d
MrCYlmz <celal63159@gmail.com>
2025-05-10 22:26:42 +0200

    refactor: clean up HomePage component by removing unused OrderSummary and simplifying code structure

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
 import { ItemCategoryEnum } from '../../api/openapi';
 import ItemCard from '../molecules/ItemCard.vue';
-import {ref, computed} from 'vue';
-import {useFetchItems} from "../../api/user/queries.ts";
-import OrderSummary from "../organism/OrderSummary.vue";
+import { ref, computed } from 'vue';
+import { useFetchItems } from "../../api/user/queries.ts";
 
-const {data: items, isLoading} = useFetchItems();
+const { data: items, isLoading } = useFetchItems();
 const selectedCategory = ref('');
 const categories = Object.values(ItemCategoryEnum);
-const filteredItems = computed(() => 
-  selectedCategory.value 
-    ? items.value?.filter(item => item.category === selectedCategory.value) 
+const filteredItems = computed(() =>
+  selectedCategory.value
+    ? items.value?.filter(item => item.category === selectedCategory.value)
     : items.value
 );
 </script>
+
 <template>
   <div class="home-page">
-    
     <v-tabs v-model="selectedCategory">
       <v-tab v-for="category in categories" :key="category" :value="category">
         {{ category }}
@@ -28,31 +27,20 @@ const filteredItems = computed(() =>
       </div>
     </div>
   </div>
-  <footer>
-    <OrderSummary />
-  </footer>
 </template>
 
 <style scoped lang="scss">
 .home-page {
-  
-.items {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  gap: 16px;
-  justify-content: center;
-}
+  .items {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    gap: 16px;
+    justify-content: center;
+  }
+
   .v-tabs {
     margin-bottom: 16px;
   }
 }
-footer {
-  position: fixed;
-  bottom: 5rem;
-  left: 0;
-  width: 100%;
-  z-index: 1000;
-}
-
 </style>
