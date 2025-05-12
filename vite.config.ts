e5aca858d0dc70bdabb614caeb85293a04269cd2
MrCYlmz <celal63159@gmail.com>
2025-05-13 00:05:48 +0200

    fix: update entry file path for output in vite config to include 'src/' prefix

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       output: {
         entryFileNames: assetInfo => {
           if (assetInfo.name === 'notfound') return '404.html'
-          return '[name].ts'
+          return 'src/[name].ts'
         }
       }
     }
