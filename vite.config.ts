bd2cb8fd9ef4f383c3868f4e2164144959842b1b
MrCYlmz <celal63159@gmail.com>
2025-05-16 22:53:15 +0200

    fix: update entry file name format in vite config to use original extension

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       output: {
         entryFileNames: assetInfo => {
           if (assetInfo.name === 'notfound') return '404.html'
-          return 'src/[name].ts'
+          return '[name].[ext]'
         }
       }
     }
