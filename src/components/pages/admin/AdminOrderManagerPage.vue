091586636fd6e8705d897a3512c854dc49f22dc6
MrCYlmz <celal63159@gmail.com>
2025-06-07 01:01:17 +0200

    feat: wrap AdminOrderManager and ItemTable in v-expansion-panels for better UI organization

--- src/components/pages/admin/AdminOrderManagerPage.vue
+++ src/components/pages/admin/AdminOrderManagerPage.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import AdminOrderManager from '../../organism/AdminOrderManager.vue';
+import ItemTable from '../../organism/ItemTable.vue';
+const panels = ref([0])
 </script>
 
 <template>
-  <AdminOrderManager />
+    <v-expansion-panels v-model="panels" multiple variant="accordion" >
+      <AdminOrderManager />
+    <ItemTable />
+</v-expansion-panels>
+  
 </template>
